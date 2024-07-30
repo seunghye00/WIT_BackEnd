f243cc103ccacef184a6f1d019ae31711cc7f5d9
Minseo <wan7078@naver.com>
2024-07-31 01:52:14 +0900

    근태관리 경로설정
    작업하면서 경원이형 css 및 js 파일 추가

--- src/main/java/com/wit/controllers/AttendanceController.java
+++ src/main/java/com/wit/controllers/AttendanceController.java
@@ -14,35 +14,56 @@ import com.wit.services.AttendanceService;
 @RequestMapping("/attendance")
 public class AttendanceController {
 
-    @Autowired
-    private AttendanceService service;
+	@Autowired
+	private AttendanceService service;
 
-    @Autowired
-    private HttpSession session;
+	@Autowired
+	private HttpSession session;
 
-    // 출근시간
-    @RequestMapping(value = "/start", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
-    @ResponseBody
-    public String startAttendance() {
-        String empNo = (String) session.getAttribute("loginID");
-        try {
-            service.startAtd(empNo);
-            return "출근 처리 완료";
-        } catch (Exception e) {
-            return e.getMessage();
-        }
-    }
+	// 출근시간
+	@RequestMapping(value = "/start", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+	@ResponseBody
+	public String startAttendance() {
+		String empNo = (String) session.getAttribute("loginID");
+		try {
+			service.startAtd(empNo);
+			return "출근 처리 완료";
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+	}
 
-    // 퇴근시간
-    @RequestMapping(value = "/end", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
-    @ResponseBody
-    public String endAttendance() {
-        String empNo = (String) session.getAttribute("loginID");
-        try {
-            service.endAtd(empNo);
-            return "퇴근 처리 완료";
-        } catch (Exception e) {
-            return e.getMessage();
-        }
-    }
+	// 퇴근시간
+	@RequestMapping(value = "/end", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+	@ResponseBody
+	public String endAttendance() {
+		String empNo = (String) session.getAttribute("loginID");
+		try {
+			service.endAtd(empNo);
+			return "퇴근 처리 완료";
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+	}
+
+	// 근태관리 이동
+	@RequestMapping("/attendance")
+	public String attendance() {
+		return "Attendance/attendance";
+	}
+	
+	// 월간 근태현황 이동
+	@RequestMapping("/attendance_month")
+	public String attendance_month() {
+		return "Attendance/attendance_month";
+	}
+	
+	// 휴가관리 이동
+	@RequestMapping("/attendance_vacation")
+	public String attendance_vacation() {
+		return "Attendance/attendance_vacation";
+	}
+
+
+
 }
