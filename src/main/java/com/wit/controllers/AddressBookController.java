23965033acfbb54e131c00b3794a837f247aef34
Moontari <shaaa6256@gmail.com>
2024-08-02 17:24:51 +0900

    주소록 개인 부분 커밋
    주소록 초성 정렬
    주소록 검색 기능
    주소록 카테고리 생성 삭제 수정 가능
    주소록 페이지네이션 검색

--- src/main/java/com/wit/controllers/AddressBookController.java
+++ src/main/java/com/wit/controllers/AddressBookController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wit.commons.BoardConfig;
@@ -18,29 +17,44 @@ import com.wit.dto.AddressBookDTO;
 import com.wit.services.AddressBookService;
 
 @Controller
-@RequestMapping("/addressbook")
+@RequestMapping("/addressbook/")
 public class AddressBookController {
 
-	@Autowired
-	private AddressBookService serv;
+    @Autowired
+    private AddressBookService serv;
 
-	@Autowired
-	private HttpSession session;
+    @Autowired
+    private HttpSession session;
 
-	@RequestMapping("/addressbook")
-    public String attendance(Model model, @RequestParam(required = false) String chosung, @RequestParam(required = false) String cpage) {
+    @RequestMapping("addressbook")
+    public String attendance(Model model, String chosung, String cpage, String category) {
+        // 세션으로 부터 사원 번호 출력
         String emp_no = (String) session.getAttribute("loginID");
+        
+        // 현재 페이지값 널값이면 1로 반환시킴  
         if (cpage == null) {
             cpage = "1";
         }
+        
+        // 현재 페이지값 데이터 타입 int로 변환  
         int cpage_num = Integer.parseInt(cpage);
 
+        // 초성 값이 널값이거나 빈값이거나 전체와 같다면 전체로 반환
+        // 그외 초성 값은 통과
         if (chosung == null || chosung.isEmpty() || "전체".equals(chosung)) {
-            chosung = "전체";
+            chosung = "";
         }
-        List<AddressBookDTO> list = serv.selectByChosung(cpage_num, emp_no, chosung);
-        int totPage = serv.totalCountPage();
 
+        // 카테고리 값이 널값이거나 빈값이면 전체로 반환
+        if (category == null || category.isEmpty() || "전체".equals(category)) {
+            category = "";
+        }
+
+        // 주소록 출력을 위해 list 배열에 담아서 전송 
+        List<Map<String, Object>> list = serv.selectByChosungAndCategory(cpage_num, emp_no, chosung, category);
+        int totPage = serv.toolCountPageByCategory(emp_no, chosung, category);
+        
+        // 모델을 통해 값을 주소록 jsp에 전달
         model.addAttribute("totPage", totPage);
         model.addAttribute("cpage", cpage_num);
         model.addAttribute("naviCountPerPage", BoardConfig.naviCountPerPage);
@@ -48,28 +62,92 @@ public class AddressBookController {
         model.addAttribute("addressBookList", list);
         return "AddressBook/addressBook";
     }
-	
-	@RequestMapping("/addressTool")
-	@ResponseBody
-	public Map<String, Object> addressBookAjax(String chosung, String cpage) {
-	    String emp_no = (String) session.getAttribute("loginID");
-	    if (cpage == null) {
-	        cpage = "1";
-	    }
-	    int cpage_num = Integer.parseInt(cpage);
 
-	    if (chosung == null || chosung.isEmpty() || "전체".equals(chosung)) {
-	        chosung = "";
-	    }
-	    List<AddressBookDTO> list = serv.selectByChosung(cpage_num, emp_no, chosung);
-	    int totPage = serv.totalCountPage();
+    @RequestMapping("addressTool")
+    @ResponseBody
+    public Map<String, Object> addressBookAjax(String chosung, String cpage, String category) {
+        // 세션으로 부터 사원 번호 출력
+        String emp_no = (String) session.getAttribute("loginID");
+        
+        // 현재 페이지값 널값이면 1로 반환시킴  
+        if (cpage == null) {
+            cpage = "1";
+        }
+        
+        // 현재 페이지값 데이터 타입 int로 변환  
+        int cpage_num = Integer.parseInt(cpage);
+        
+        // 초성 값이 널값이거나 빈값이거나 전체와 같다면 전체로 반환
+        // 그외 초성 값은 통과
+        if (chosung == null || chosung.isEmpty() || "전체".equals(chosung)) {
+            chosung = "";
+        }
+        
+        // 카테고리 값이 널값이거나 빈값이면 전체로 반환
+        if (category == null || category.isEmpty() || "전체".equals(category)) {
+            category = "";
+        }
+        System.out.println(chosung+" / " +category);
+        // 주소록 출력을 위해 list 배열에 담아서 전송 
+        List<Map<String, Object>> list = serv.selectByChosungAndCategory(cpage_num, emp_no, chosung, category);
+        int totPage = serv.toolCountPageByCategory(emp_no, chosung, category);
+        
+        // 모델을 통해 값을 주소록 ajax 요청에 전달
+        Map<String, Object> response = new HashMap<>();
+        response.put("totPage", totPage);
+        response.put("cpage", cpage_num);
+        response.put("naviCountPerPage", BoardConfig.naviCountPerPage);
+        response.put("recordCountPerPage", BoardConfig.recordCountPerPage);
+        response.put("addressBookList", list);
+        return response;
+    }
 
-	    Map<String, Object> response = new HashMap<>();
-	    response.put("totPage", totPage);
-	    response.put("cpage", cpage_num);
-	    response.put("naviCountPerPage", BoardConfig.naviCountPerPage);
-	    response.put("recordCountPerPage", BoardConfig.recordCountPerPage);
-	    response.put("addressBookList", list);
-	    return response;
-	}
+    // 게시글 검색
+    @RequestMapping("search")
+    @ResponseBody
+    public Map<String, Object> search(String keyword, String cpage) throws Exception {
+        if (cpage == null) {
+            cpage = "1";
+        }
+        int cpage_num = Integer.parseInt(cpage);
+        List<Map<String, Object>> list = serv.selectByCon(keyword, cpage_num);
+        int totPage = serv.totalCountPageSearch(keyword);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("totPage", totPage);
+        response.put("cpage", cpage_num);
+        response.put("naviCountPerPage", BoardConfig.naviCountPerPage);
+        response.put("recordCountPerPage", BoardConfig.recordCountPerPage);
+        response.put("addressBookList", list);
+        return response;
+    }
+
+    // 카테고리
+    @RequestMapping("getCategories")
+    @ResponseBody
+    public List<String> getCategories() {
+        String emp_no = (String) session.getAttribute("loginID");
+        return serv.getCategories(emp_no);
+    }
+
+    @RequestMapping("addCategory")
+    @ResponseBody
+    public boolean addCategory(String category) {
+        String emp_no = (String) session.getAttribute("loginID");
+        return serv.addCategory(emp_no, category);
+    }
+
+    @RequestMapping("editCategory")
+    @ResponseBody
+    public boolean editCategory(String oldCategory, String newCategory) {
+        String emp_no = (String) session.getAttribute("loginID");
+        return serv.editCategory(emp_no, oldCategory, newCategory);
+    }
+
+    @RequestMapping("deleteCategory")
+    @ResponseBody
+    public boolean deleteCategory(String category) {
+        String emp_no = (String) session.getAttribute("loginID");
+        return serv.deleteCategory(emp_no, category);
+    }
 }
--- src/main/java/com/wit/services/AddressBookService.java
+++ src/main/java/com/wit/services/AddressBookService.java
@@ -19,20 +19,90 @@ public class AddressBookService {
 
 	// 이름의 초성으로 레코드 조회
 	@Transactional
-	public List<AddressBookDTO> selectByChosung(int cpage, String emp_no, String chosung) {
-        Map<String, Object> params = new HashMap<>();
-        int startNum = (cpage - 1) * BoardConfig.recordCountPerPage + 1;
-        int endNum = cpage * BoardConfig.recordCountPerPage;
-        params.put("emp_no", emp_no);
-        params.put("chosung", chosung);
-        params.put("startNum", startNum);
-        params.put("endNum", endNum);
+	public List<Map<String, Object>> selectByChosungAndCategory(int cpage, String emp_no, String chosung, String category) {
+	    int startNum = (cpage - 1) * BoardConfig.recordCountPerPage + 1;
+	    int endNum = cpage * BoardConfig.recordCountPerPage;
 
-        return dao.selectByChosung(params);
+	    Map<String, Object> params = new HashMap<>();
+	    params.put("emp_no", emp_no);
+	    params.put("chosung", chosung);
+	    params.put("category", category);
+	    params.put("startNum", startNum);
+	    params.put("endNum", endNum);
+
+	    return dao.selectByChosungAndCategory(params);
+	}
+	
+	// 주소록 초성 별 카운트 값 조회
+	@Transactional
+	public int toolCountPageByCategory(String emp_no, String chosung, String category) {
+	    Map<String, Object> params = new HashMap<>();
+	    params.put("emp_no", emp_no);
+	    params.put("chosung", chosung);
+	    params.put("category", category);
+	    return dao.toolCountPageByCategory(params);
+	}
+	
+	// 주소록 검색 카운트 값 조회
+	@Transactional
+	public int totalCountPageSearch(String keyword) {
+		return dao.totalCountPageSearch(keyword);
+	}
+	
+	// 주소록 검색값 레코드 조회
+	@Transactional
+	public List<Map<String, Object>> selectByCon(String keyword, int cpage) {
+		return dao.selectByCon(keyword, cpage * BoardConfig.recordCountPerPage - (BoardConfig.recordCountPerPage - 1),
+				cpage * BoardConfig.recordCountPerPage);
+	}
+	
+	// 콘텐츠 등록
+//	public void addContact(AddressBookDTO contact) {
+//	    dao.insertContact(contact);
+//	}
+	
+	// 카테고리
+    // 카테고리 가져오기
+    @Transactional
+    public List<String> getCategories(String emp_no) {
+        return dao.getCategories(emp_no);
     }
 
-	@Transactional
-	public int totalCountPage() {
-		return dao.totalCountPage();
-	}
+    @Transactional
+    public boolean addCategory(String emp_no, String category) {
+        if (category == null) {
+            category = "";
+        }
+        Map<String, Object> params = new HashMap<>();
+        params.put("emp_no", emp_no);
+        params.put("category", category);
+        return dao.addCategory(params) > 0;
+    }
+
+    @Transactional
+    public boolean editCategory(String emp_no, String originalCategory, String category) {
+        if (originalCategory == null) {
+            originalCategory = "";
+        }
+        if (category == null) {
+            category = "";
+        }
+        Map<String, Object> params = new HashMap<>();
+        params.put("emp_no", emp_no);
+        params.put("oldCategory", originalCategory);
+        params.put("newCategory", category);
+        return dao.editCategory(params) > 0;
+    }
+
+    @Transactional
+    public boolean deleteCategory(String emp_no, String category) {
+        if (category == null) {
+            category = "";
+        }
+        Map<String, Object> params = new HashMap<>();
+        params.put("emp_no", emp_no);
+        params.put("category", category);
+        return dao.deleteCategory(params) > 0;
+    }
+
 }
