cfb207ffd602a0537c59850e16c4fc17ea40d9c6
Minseo <wan7078@naver.com>
2024-08-19 13:47:41 +0900

    백업 브랜치에 있던 휴가 코드 메인으로 옮김
    페이지 네비만 하면 끝

--- src/main/java/com/wit/services/AnnualLeaveService.java
+++ src/main/java/com/wit/services/AnnualLeaveService.java
@@ -2,6 +2,7 @@ package com.wit.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.wit.dao.AnnualLeaveDAO;
 import com.wit.dto.AnnualLeaveDTO;
@@ -13,21 +14,44 @@ import java.util.List;
 @Service
 public class AnnualLeaveService {
 
-    @Autowired
-    private AnnualLeaveDAO dao;
+	@Autowired
+	private AnnualLeaveDAO dao;
 
-    // 직원의 연차 정보를 가져옴
-    public AnnualLeaveDTO getAnnualLeaveByEmpNo(String empNo) {
-        return dao.getAnnualLeaveByEmpNo(empNo);
-    }
+	// 직원의 연차 정보를 가져옴
+	public AnnualLeaveDTO getAnnualLeaveByEmpNo(String empNo) {
+		return dao.getAnnualLeaveByEmpNo(empNo);
+	}
 
-    // 상태가 완료된 직원의 휴가 신청 내역을 반환
-    public List<LeaveRequestDTO> getApprovedLeaveRequestsByEmpNo(String empNo) {
-        return dao.getApprovedLeaveRequestsByEmpNo(empNo);
-    }
+	// 상태가 완료된 직원의 휴가 신청 내역을 반환
+	public List<LeaveRequestDTO> getApprovedLeaveRequestsByEmpNo(String empNo) {
+		return dao.getApprovedLeaveRequestsByEmpNo(empNo);
+	}
 
-    // 직원 정보 조회
-    public EmployeeDTO employeeInfo(String empNo) {
-        return dao.employeeInfo(empNo);
-    }
+	// 직원 정보 조회
+	public EmployeeDTO employeeInfo(String empNo) {
+		return dao.employeeInfo(empNo);
+	}
+
+	// 휴가 승인 처리 (연차 차감 및 로그 기록)
+	@Transactional
+	public void processLeaveApproval(LeaveRequestDTO leaveRequest) {
+		int documentSeq = leaveRequest.getDocument_seq();
+
+		// document_seq를 통해 emp_no를 조회
+		String empNo = dao.getEmpNoByDocumentSeq(documentSeq);
+
+		int daysUsed = Math.round(leaveRequest.getRequest_leave_days());
+
+		// 연차 정보 가져오기
+		AnnualLeaveDTO annualLeave = dao.getAnnualLeaveByEmpNo(empNo);
+
+		if (annualLeave != null) {
+			// 사용한 연차 차감
+			int updatedUseNum = annualLeave.getUse_num() + daysUsed;
+			dao.updateAnnualLeaveUsage(empNo, updatedUseNum);
+
+			// 연차 로그 기록
+			dao.insertAnnualLeaveLog(empNo, annualLeave.getAnnual_leave_seq(), leaveRequest.getDocument_seq());
+		}
+	}
 }
